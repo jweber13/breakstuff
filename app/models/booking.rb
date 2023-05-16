0bcfb39b2a8ffdd390c651d79b9666c72f325e26
Mathieu Belson <mathieu.belson@gmail.com>
2023-05-16 13:35:09 +0900

    Revert "seeds added"
    This reverts commit 5da571a55e90337bb43b43bbda37516da815cd84.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,6 @@ class Booking < ApplicationRecord
   belongs_to :room
 
   validates_presence_of :date, :number_of_people, :status
-  validates :number_of_people, numericality: { only_integer: true }
+  validates :number_of_people, :status, numericality: { only_integer: true }
   enum :status, { requested: 0, accepted: 1, rejected: 2 }
 end
