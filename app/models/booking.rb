bc3491ec1527fdaf24bee36c4492721f4c2e8348
joe weber <jweber4213@gmail.com>
2023-05-18 17:30:59 +0900

    updated bookings model validations against room capacity and user (owners can't book)

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,4 +5,23 @@ class Booking < ApplicationRecord
   validates_presence_of :date, :number_of_people, :status
   validates :number_of_people, numericality: { only_integer: true }
   enum :status, { requested: 0, accepted: 1, rejected: 2 }
+
+  validate :different_users
+  validate :compare_capacity
+
+  private
+
+  def different_users
+    if user == room.user
+      errors.add(:user, "can't be the same as the room's user")
+    end
+  end
+
+  def compare_capacity
+    if number_of_people > room.capacity
+      errors.add(:number_of_people, "can't go over capacity")
+    end
+  end
+
+
 end
