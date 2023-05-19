e0f20276ba10ebcc21131f5bed3e5947cc508ba7
Ryo Tamiya <ryo.tamiyaayimat.oyr@gmail.com>
2023-05-19 13:53:53 +0900

    add geocoding to rooms#index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_18_100320) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_19_032648) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -74,6 +74,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_18_100320) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_rooms_on_user_id"
   end
 
