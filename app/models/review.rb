f573ace8bce941f22573e3dcdbb939ec598220ab
joe weber <jweber4213@gmail.com>
2023-05-19 11:02:35 +0900

    updated views & navbar

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,12 +3,20 @@ class Review < ApplicationRecord
   belongs_to :user
 
   validates :content, presence: true
+  # validate :different_users
+  # validate :has_bookedx
 
-  private
+  # private
 
-  def different_users
-    if user == room.user
-      errors.add(:user, "can't be the same as the room's user")
-    end
-  end
+  # def different_users
+  #   if user == room.user
+  #     errors.add(:user, "can't be the same as the room's user")
+  #   end
+  # end
+
+  # def has_booked
+  #   if room.bookings.all? { |booking| booking.user != user }
+  #     errors.add(:user, "must have booked before")
+  #   end
+  # end
 end
